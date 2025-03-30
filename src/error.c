0ea1064a9e3c90d0b1370f91a286a15d4cc53b9b
Zaïnab <zeinab0695@gmail.com>
2025-03-30 22:18:49 +0200

    hehe

--- src/error.c
+++ src/error.c
@@ -16,9 +16,9 @@ void    error_exit(const char *msg, int exit_code)
 void    handle_error(int argc, char **argv)
 {
     if (argc < 5)
-        error_exit("Invalid number of arguments", 1);
+        error_exit("Invalid number of arguments :(", 1);
     if (access(argv[1], R_OK) == -1)
-        error_exit("Infile not accessible in reading mode", 126);
+        error_exit("Infile not accessible in reading mode :(", 126);
     if (access(argv[4], W_OK) == -1 && access(argv[4], F_OK) == -1)
-        error_exit("Outfile not accessible in writing mode", 126);
+        error_exit("Outfile not accessible in writing mode :(", 126);
 }
--- src/parsing.c
+++ src/parsing.c
@@ -1,22 +1,49 @@
 
 # include "../include/pipex.h"
 
-void    handle_arguments(int argc, char **argv)
+void    handle_arguments(int argc, char **argv, char **cmd1, char **cmd2)
 {
-    char **cmd1;
-    char **cmd2;
-
     cmd1 = ft_split(argv[2], " ");
-    cmd2 = ft_split(argv[4], " ");
+    cmd2 = ft_split(argv[3], " ");
 }
 
-void    handle_files()
+void    handle_files(int argc, char **argv, int *fd_infile, int *fd_outfile, int fd[2])
 {
-    int fd_infile;
-    int fd_outfile;
-
+    if (pipe(fd) == -1)
+        error_exit("Pipe creation failed :(", 1);
     fd_infile = open(argv[1], O_RDONLY);
-    fd_outfile = open(argv[5], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    fd_outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd_infile < 0 || fd_outfile < 0)
-        error_exit("File error", 1);
+        error_exit("File error : (", 1);
+}
+
+void    execute_commands(int fd[2], int fd_infile, int fd_outfile, char **cmd1, char **cmd2)
+{
+    int pid1;
+    int pid2;
+
+    pid1 = fork();
+    if (pid1 < 0)
+        error_exit("Oopsie fork failed :(", 2);
+    if (pid1 == 0)
+    {
+        dup2(fd_infile, STDIN_FILENO);
+        dup2(fd[1], STDOUT_FILENO);
+        execve(cmd1[0], cmd1, NULL);
+        error_exit("Execve failed :(", 1);
+    }
+    pid2 = fork();
+    if (pid2 < 0)
+        error_exit("Oopsie fork failed :( ", 2);
+    if (pid2 == 0)
+    {
+        dup2(fd[0], STDIN_FILENO);
+        dup2(fd_outfile, STDOUT_FILENO);
+        execve(cmd2[0], cmd2, NULL);
+        error_exit("Execve failed :(", 1);
+    }
+    close(fd[0]);
+    close(fd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 }
